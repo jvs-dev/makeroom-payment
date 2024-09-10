3fba3134d3d944d4d37289aed049e03b3e74a984
joão vitor <shepherdcom12@gmail.com>
2024-09-10 03:29:45 -0300

    first commit

---
+++ pages/api/createpay.js
@@ -0,0 +1,38 @@
+const mercadopago = require('mercadopago');
+const { MercadoPagoConfig, Payment } = mercadopago;
+
+const client = new MercadoPagoConfig({ accessToken: `${process.env.MAKEROOM_KEY}` });
+const payment = new Payment(client);
+
+function expiration() {
+    let currentDate = new Date();
+    let expiration = new Date(currentDate.getTime() + 10 * 60000);
+    return expiration
+}
+
+const init = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(200).end();
+        return;
+    }
+    try {
+        let result = await payment.create({
+            body: {
+                transaction_amount: Number(req.body.value),
+                description: `Compra de ${req.body.items} no Makeroom`,
+                payment_method_id: 'pix',
+                date_of_expiration: expiration(),
+                payer: {
+                    email: `${req.body.payerEmail}`
+                },
+            }
+        });
+        res.json({ result });
+    } catch (error) {
+        res.json({ error });
+    }    
+};
+export default init;
---
+++ pages/api/getPay.js
@@ -0,0 +1,25 @@
+const mercadopago = require('mercadopago');
+const { MercadoPagoConfig, Payment } = mercadopago;
+
+const client = new MercadoPagoConfig({ accessToken: `${process.env.MAKEROOM_KEY}` });
+const payment = new Payment(client);
+
+const init = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(200).end();
+        return;
+    }
+    try {
+        let result = await payment.get({
+            id: `${req.body.payId}`,
+        });
+        res.json({ result });
+    } catch (error) {
+        res.json({ error });
+    }
+};
+
+export default init;
